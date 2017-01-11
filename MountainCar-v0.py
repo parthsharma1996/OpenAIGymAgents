1116af65417d2a8d3ff6e25bacbf9c3ca3d7456f
parthsharma1996@gmail.com <parthsharma1996@gmail.com>
2017-01-11 20:54:22 +0530

    Added implmentations of Acrobat and Mountain car. Mountain car doesn't work yet.

---
+++ MountainCar-v0.py
@@ -0,0 +1,70 @@
+import gym
+import numpy as np
+import math
+import random
+
+obs_dim = 2
+action_dim = 3 
+learning_rate = 0.001
+max_trials = 1000000
+env = gym.make('MountainCar-v0')
+max_steps = 10
+r_list= []
+epr_list = []
+w = np.zeros((action_dim,obs_dim))
+ep_start = 1
+ep_end = 0.1
+gamma = 0.95
+print_freq = 100
+solv_req = 1
+record =True
+
+if record:
+    env.monitor.start('/tmp/MountainCar-experiment-1',force=True)
+    
+def updateWeights(prev_obs,next_obs,action,r):
+    global w #asking python to use the global variable w
+    prev_obs = np.asarray(prev_obs)
+    next_obs = np.asarray(next_obs)
+    q_target = r + gamma * np.amax(np.dot(w , next_obs))
+    q_present = np.dot(w,prev_obs)[action]
+    w[action,:] = w[action,:] + learning_rate * (q_target - q_present)*prev_obs
+
+def selectAction(epsilon,obs):
+    global w
+    obs = np.asarray(obs)
+    if np.random.rand() < epsilon:
+        action = random.randint(0,1)
+    else:
+        action = np.argmax(np.dot(w,obs))
+
+    return action
+
+
+for trial_num in range(0,max_trials):
+    
+    obs = env.reset()
+    prev_obs = obs
+    
+    for num_steps in range(0,max_steps):
+
+        action = selectAction(ep_end+1/(trial_num+1),obs)
+        obs,r,done,info = env.step(action)
+        updateWeights(prev_obs,obs,action,r)
+        r_list.append(r)
+        prev_obs = obs
+        
+        if done:
+            epr_list.append(sum(r_list))
+            r_list = []
+            break
+    if trial_num%print_freq == 0:
+        print("Average reward in the last 100 epsiodes and ep num",sum(epr_list[-100:])/100,trial_num)
+
+    if sum(epr_list[-100:])/100 >=solv_req:
+        print(trial_num)
+        break
+
+if record:
+    env.monitor.close()
+    gym.upload('/tmp/MountainCar-experiment-1')
